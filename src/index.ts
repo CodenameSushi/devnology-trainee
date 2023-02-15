b0df341b8a719ae25a1946ba8456ae27392de50c
CodenameSushi <fhenrique08@gmail.com>
2023-02-15 19:47:50 -0300

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, {Request, Response} from 'express';
 import cors from "cors";
 import { articleRouter } from './router/articleRouter';
+import dotenv from 'dotenv';
 
+dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003");
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
 });
 
 app.use("/articles", articleRouter)
